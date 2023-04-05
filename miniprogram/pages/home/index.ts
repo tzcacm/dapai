2861a791f34c5f27cc3bdc09e574a7ba09d93e2e
tzcacm <>
2023-04-05 18:17:54 +0800

    优化

--- miniprogram/pages/home/index.ts
+++ miniprogram/pages/home/index.ts
@@ -23,76 +23,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   async onLoad() {
-    let res: any = await wxLogin()
+    this.getInitData()
+  },
 
+  // 登录授权
+  async getInitData() {
+    let res: any = await wxLogin()
     post('getAccetsToken', { code: res?.code }).then(data => {
       console.log(data)
     })
   },
 
-  toOptionDetail() { },
-
-  showBcgImgArea() {
-
-  },
-  // 授权用户信息
-  getUserProfile() {
-    getUserProfile().then((res: any) => {
-      console.log(res)
-      this.setData({
-        userinfo: res.userInfo,
-        hasUserInfo: true
-      })
-      wx.setStorageSync('userinfo', res.userInfo)
-    })
+  // 创建房间
+  toOptionDetail() {
+    wx.navigateTo({ url: `/pages/room/index` })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+  // 扫码进房间
+  toShareDetail() {
 
   }
 })
--- miniprogram/pages/person/index.ts
+++ miniprogram/pages/person/index.ts
@@ -1,10 +1,43 @@
+import { post } from '../../api/http'
+
 Page({
   data: {
     showLoading: false,
     showLoginBox: false,
-    animation: false
+    animation: false,
+    userInfo: {}
   },
   onLoad() {
 
+  },
+  // 弹出授权窗口
+  getUserInfoData() {
+    // this.setData({ showLoginBox: true })
+  },
+  // 拒绝授权
+  cancelPush() {
+    this.setData({ showLoginBox: false })
+  },
+  // 接受授权
+  surePushTwo() {
+    wx.getUserProfile({
+      lang: 'zh_CN',
+      desc: '用于完善会员资料',
+      success: (res) => {
+        this.setData({ showLoginBox: false })
+        post('saveUserinfo', { ...res.userInfo }).then(() => {
+          this.getInitData()
+        })
+      },
+      fail: (err) => {
+        console.log(err)
+      }
+    })
+  },
+  // 初始化数据-获取用户信息
+  getInitData() {
+    post('getUserinfo').then(res => {
+      console.log(res)
+    })
   },
 })
