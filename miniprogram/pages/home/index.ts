cb1b51e5190e138ccf28ab668865f41b055adabe
tzcacm <>
2023-04-05 20:50:46 +0800

    优化

--- miniprogram/pages/home/index.ts
+++ miniprogram/pages/home/index.ts
@@ -1,6 +1,7 @@
 // pages/home/index.ts
 import { post } from '../../api/http'
 import { wxLogin, getUserProfile } from '../../utils/tool'
+import { centerLetter } from '../../utils/license';
 
 Page({
 
@@ -16,7 +17,12 @@ Page({
       nickName: '未授权'
     },
     hasUserInfo: false,
-    canIUseGetUserProfile: true
+    canIUseGetUserProfile: true,
+    fadeshow: false,
+    isShowRoomNumber: false,
+    roomListArr: centerLetter, // 号码列表
+    roomNumber: [],
+    roomIndex: 0
   },
 
   /**
@@ -39,8 +45,34 @@ Page({
     wx.navigateTo({ url: `/pages/room/index` })
   },
 
-  // 扫码进房间
-  toShareDetail() {
+  // 加入房间
+  toRoomDetail() {
+    this.setData({ isShowRoomNumber: true, fadeshow: true })
+  },
 
+  // 输入号码
+  getLicenseNumber(e) {
+    const { value } = e.currentTarget.dataset || {}
+    const { roomIndex, roomNumber } = this.data || {}
+    if (roomIndex >= 4) return
+    // if (roomIndex == 0) {
+    //   this.setData({ cartLicenseArr: FistLetter })
+    // } else if (roomIndex >= 1 && roomIndex < minIndex) {
+    //   this.setData({ cartLicenseArr: centerLetter })
+    // } else {
+    //   this.setData({ cartLicenseArr: lastLetter })
+    // }
+    // this.setData({
+    //   roomNumber: roomNumber + value,
+    //   roomIndex: roomIndex + 1,
+    // })
+  },
+
+  // 关闭房间
+  cacelRoomDetail() {
+    this.setData({ fadeshow: false })
+    setTimeout(() => {
+      this.setData({ isShowRoomNumber: false })
+    }, 250)
   }
 })
